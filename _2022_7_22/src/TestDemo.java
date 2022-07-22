38a5e851392670d9f4d6f9975242802497ad22ff
xin-xiangrong <1005037512@qq.com>
2022-07-22 17:38:08 +0800

    练习

---
+++ _2022_7_22/src/TestDemo.java
@@ -0,0 +1,10 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+        int ret = a+b;
+        System.out.println(ret);
+    }
+}
