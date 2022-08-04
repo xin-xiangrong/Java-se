2fccb9397be45e174221d7ecbe8c45206700cc5f
xin-xiangrong <1005037512@qq.com>
2022-08-04 17:37:15 +0800

    练习

---
+++ J-20220724-firstlookJava/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(10);
+        System.out.println(10);
+
+
+        for (int j = 0; j < 100; j++) {
+            
+        }
+    }
+}
