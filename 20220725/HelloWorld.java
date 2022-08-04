d3895166dc44defc9627d55a00d08983e7ee3e6b
xin-xiangrong <1005037512@qq.com>
2022-08-04 17:36:27 +0800

    练习

---
+++ 20220725/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @version v1.0.0
+* @author 106程序员
+ */
+public class HelloWorld{
+	/**
+	 * hhhh
+	 * @param args [hhhh]
+	 */
+	public static void main(String[] args){
+		System.out.println("hello");
+	}
+
+}
