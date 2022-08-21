3292bef9a9f17f5a2f81459fd3a0a26bc928d1ef
xin-xiangrong <1005037512@qq.com>
2022-08-21 13:55:14 +0800

    练习

--- Blog20220821/src/Test2.java
+++ Blog20220821/src/Test2.java
@@ -29,7 +29,7 @@ class Person implements Cloneable{
         return "Person{" +
                 "id=" + id +
                 ", money=" + money.m +
-                '}';
+                '}';   
     }
 }
 public class Test2 {
